ad50a8e984fd35d2a9de8fc2e816a4cd83eb5a71
freitzzz <1160907@isep.ipp.pt>
2022-07-08 00:38:18 +0100

    expansion tiles view ready

--- example/lib/views/design_system/design_system_expansion_tiles_view.dart
+++ example/lib/views/design_system/design_system_expansion_tiles_view.dart
@@ -59,6 +59,64 @@ final _expansionTilesLabels = <String, FHDSExpansionTile>{
       ),
     ],
   ),
+  'Expanded': const FHDSExpansionTile(
+    expanded: true,
+    onExpansionChanged: _onExpansionChanged,
+    title: [
+      FHDSText(
+        'Board member',
+        prefixIcon: FHDSIcons.boardMembers,
+        suffixIcon: FHDSIcons.copy,
+      ),
+      FHDSText(
+        '+47 444 44 444',
+        prefixIcon: FHDSIcons.phone,
+        suffixIcon: FHDSIcons.copy,
+      ),
+      FHDSText(
+        'Priority 1',
+      ),
+      FHDSText(
+        'mario@castle.com',
+        prefixIcon: FHDSIcons.email,
+        suffixIcon: FHDSIcons.copy,
+      ),
+    ],
+    content: {
+      'UPDATED AT': FHDSText(
+        '01.01.1970',
+      ),
+      'ADDRESS': FHDSText(
+        '2_0',
+        suffixIcon: FHDSIcons.copy,
+      ),
+      'DEVICE ID': FHDSText(
+        '1',
+      ),
+    },
+    primaryActions: [
+      FHDSChip(
+        label: 'EDIT',
+        onPressed: _onChipPressed,
+      ),
+      FHDSChip(
+        label: 'DELETE',
+        onPressed: _onChipPressed,
+      ),
+    ],
+    secondaryActions: [
+      FHDSChip(
+        label: 'TIMELINE',
+        onPressed: _onChipPressed,
+        suffixIcon: FHDSIcons.forward,
+      ),
+      FHDSChip(
+        label: '8 NOTES',
+        suffixIcon: FHDSIcons.forward,
+        onPressed: _onChipPressed,
+      ),
+    ],
+  ),
 };
 
 class DesignSystemExpansionTilesView extends StatelessWidget {
