a0c58db378605e0dbdb3689290bdef0e99c0f952
freitzzz <1160907@isep.ipp.pt>
2022-07-07 21:01:39 +0100

    chips view ready

--- example/lib/views/design_system/design_system_chips_view.dart
+++ example/lib/views/design_system/design_system_chips_view.dart
@@ -1,6 +1,34 @@
 import 'package:fhds/fhds.dart';
 import 'package:flutter/material.dart';
 
+const _chipsLabels = <String, FHDSChip>{
+  'Single label': FHDSChip(
+    label: 'LABEL',
+    onPressed: _onChipPressed,
+  ),
+  'With prefix icon': FHDSChip(
+    label: 'LABEL',
+    prefixIcon: FHDSIcons.plus,
+    onPressed: _onChipPressed,
+  ),
+  'With suffix icon': FHDSChip(
+    label: 'LABEL',
+    suffixIcon: FHDSIcons.forward,
+    onPressed: _onChipPressed,
+  ),
+  'With prefix and suffix icon': FHDSChip(
+    label: 'LABEL',
+    prefixIcon: FHDSIcons.plus,
+    suffixIcon: FHDSIcons.forward,
+    onPressed: _onChipPressed,
+  ),
+  'Secondary': FHDSChip(
+    label: 'LABEL',
+    onPressed: _onChipPressed,
+    secondary: true,
+  ),
+};
+
 class DesignSystemChipsView extends StatelessWidget {
   const DesignSystemChipsView({
     super.key,
@@ -12,6 +40,25 @@ class DesignSystemChipsView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Chips'),
       ),
+      body: ListView.separated(
+        primary: false,
+        itemCount: _chipsLabels.length,
+        itemBuilder: (context, index) {
+          final entry = _chipsLabels.entries.elementAt(index);
+
+          return Column(
+            children: [
+              entry.value,
+              FHDSText(entry.key),
+            ],
+          );
+        },
+        separatorBuilder: (context, _) {
+          return const Divider();
+        },
+      ),
     );
   }
 }
+
+void _onChipPressed() {}
