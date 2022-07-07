937fab86996fafe619cca8176b743ce2c0f9cfd7
freitzzz <1160907@isep.ipp.pt>
2022-07-07 20:29:51 +0100

    icons view ready

--- example/lib/views/design_system/design_system_icons_view.dart
+++ example/lib/views/design_system/design_system_icons_view.dart
@@ -1,6 +1,21 @@
 import 'package:fhds/fhds.dart';
 import 'package:flutter/material.dart';
 
+const _iconsLabels = <String, FHDSIcons>{
+  'Board Members': FHDSIcons.boardMembers,
+  'Check': FHDSIcons.check,
+  'Common Area': FHDSIcons.commonArea,
+  'Copy': FHDSIcons.copy,
+  'E-mail': FHDSIcons.email,
+  'Exclamation Mark': FHDSIcons.exclamation,
+  'Forward': FHDSIcons.forward,
+  'Phone': FHDSIcons.phone,
+  'Plus': FHDSIcons.plus,
+  'Smoke Detector': FHDSIcons.smokeDetector,
+  'Up': FHDSIcons.up,
+  'Water Leak Detector': FHDSIcons.waterLeakDetector,
+};
+
 class DesignSystemIconsView extends StatelessWidget {
   const DesignSystemIconsView({
     super.key,
@@ -11,6 +26,23 @@ class DesignSystemIconsView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Icons'),
+      ),
+      body: ListView.separated(
+        primary: false,
+        itemCount: _iconsLabels.length,
+        itemBuilder: (context, index) {
+          final entry = _iconsLabels.entries.elementAt(index);
+
+          return Column(
+            children: [
+              Icon(entry.value),
+              FHDSText(entry.key),
+            ],
+          );
+        },
+        separatorBuilder: (context, _) {
+          return const Divider();
+        },
       ),
     );
   }
